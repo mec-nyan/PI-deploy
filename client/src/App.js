9faf2c2f79b18dc9da9a896bb3883a090e971746
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-21 21:17:03 -0300

    landing page elements and animations

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import btn from './img/enterButton.png';
+import magicBlue from './img/magicCircleBlue.png';
+import magicMagenta from './img/magicCircleMagenta.png';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>MEC</h1>
       </header>
+      <div className='magic'>
+        <img className='magenta' src={magicMagenta} />
+      </div>
+      <div className='magic'>
+        <img className='blue' src={magicBlue} />
+      </div>
+      <div className='enterBtn'>
+        <img src={btn} />
+      </div>
       <div className="Slider">
         <div class="big">
           <div class="slide"></div>
