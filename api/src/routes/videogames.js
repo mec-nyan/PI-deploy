6be99aa617500b235de9118d1441c40489468184
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-26 23:17:50 -0300

    videogame route now asks the api

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -6,11 +6,11 @@ const process = require('process');
 const axios = require('axios');
 require('dotenv').config();
 const { KEY } = process.env;
-const url = ' https://api.rawg.io/api/games';
+const url = 'https://api.rawg.io/api/games';
 
 
 
-
+/* >> No hay que usar la db local???
 router.get('/', async function(req, res) {
   // GET /videogames?name="...":
   // Obtener un listado de las primeros 15 videojuegos que contengan la palabra ingresada como query parameter
@@ -40,6 +40,33 @@ router.get('/', async function(req, res) {
       output.push({ id, name, slug, genres });
     });
     return res.json({ output });
+  }
+});
+*/ 
+
+router.get('/', async function(req, res) {
+  // filter by name 
+  let { name } = req.query;
+  let games;
+  if (name) {
+    games = await axios.get(`${url}?search=${name}&key=${KEY}`);
+  } else {
+    games = await axios.get(`${url}?key=${KEY}`);
+  }
+  let out = games.data.results.map(function(g) {
+    return {
+      name: g.name,
+      rating: g.rating,
+      genres: g.genres.map( genre => genre.name ),
+      image: g.background_image,
+    };
+  });
+
+  console.log(out.length);
+  if (games.data) {
+    return res.status(200).json(out);
+  } else {
+    return res.status(404).json({msg: 'Not found'});
   }
 });
 
