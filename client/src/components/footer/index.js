2f970b5794d13d0f83aefbc23d27637bf7a429ae
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-30 17:15:26 -0300

    simple text added to footer component

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -5,8 +5,19 @@ function Footer() {
 
   return (
     <div className='foot'>
-      <div>
-        <small>This is a footer.</small>
+      <div className='left'>
+        <p>Lead web developer</p> 
+        <p>Cascade Style Sheets by</p>
+        <p>Backend design</p>
+      </div>
+      <div className='right'>
+        <p>Mariano Macri</p>
+        <p>Mariano Eloy Collantes</p> 
+        <p>&lt; mec-nyan /&gt;</p>
+      </div>
+
+      <div className='henry'>
+        <p>&iexcl;Soy Henry!</p>
       </div>
     </div>
   );
