c14c34fb5ba0a0947975ddb92277f1fd0c4ea55a
mec-nyan <marianoraspberrypi@gmail.com>
2021-10-01 16:20:59 -0300

    models/routes tests

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -1,7 +1,7 @@
-const { Videogame, conn } = require('../../src/db.js');
+const { Genre, Platform, Videogame, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('Videogame model', () => {
+describe('Model: Videogame', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
@@ -14,8 +14,48 @@ describe('Videogame model', () => {
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
+      it('should work when it\'s a valid name', () => {
         Videogame.create({ name: 'Super Mario Bros' });
+      });
+    });
+  });
+});
+
+describe('Model: Genre', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Videogame.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        Genre.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when it\'s a valid name', () => {
+        Genre.create({ name: 'Action' });
+      });
+    });
+  });
+});
+
+describe('Model: Platform', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Videogame.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        Platform.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when it\'s a valid name', () => {
+        Platform.create({ name: 'Wii' });
       });
     });
   });
--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -4,21 +4,72 @@ const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Videogame, conn } = require('../../src/db.js');
 
-const agent = session(app);
-const videogame = {
-  name: 'Super Mario Bros',
+let agent = null;
+
+const aGame = {
+  name: 'a game',
+  rating: '0',
+  description: 'this is a game. well, not really.',
+  released: '1-1-1111',
+  genres: [],
+  platforms: [],
 };
+
+const bGame = { ...aGame };
 
 describe('Videogame routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
+
   beforeEach(() => Videogame.sync({ force: true })
-    .then(() => Videogame.create(videogame)));
+    .then(() => Videogame.create(aGame)));
+
+  beforeEach(function () {
+    agent = session(app);
+  });
+  
   describe('GET /videogames', () => {
-    it('should get 200', () =>
-      agent.get('/videogames').expect(200)
-    );
+    it('should get 200', (done) => {
+      agent.get('/videogames').expect(200);
+      done();
+    });
+  });
+
+  describe('GET /videogame/1', () => {
+    it('status should be 200', (done) => {
+      agent.get('/videogame/1')
+        .then(out => expect(out.status).to.equal(200))
+        .catch();
+      done();
+    });
+    it('id should be 1', (done) => {
+      agent.get('/videogame/1')
+        .then(out => expect(out.body.id).to.equal(1))
+        .catch();
+      done();
+    });
+    it('name should be "D/Generation HD"', (done) => {
+      agent.get('/videogame/1')
+        .then(out => expect(out.body.name).to.equal("D/Generation HD"))
+        .catch();
+      done();
+    });
+  });
+
+  describe('POST /videogame', () => {
+    it('should return 200', (done) => {
+      agent.post('/videogame')
+        .send(aGame)
+        .expect(200)
+        .end(done);
+    });
+    it('should return 407', (done) => {
+      agent.post('/videogame')
+        .send({name: 'a game'})
+        .expect(407)
+        .end(done);
+    });
   });
 });
